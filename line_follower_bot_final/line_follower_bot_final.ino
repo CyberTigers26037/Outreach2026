8d23e3793bf9b48b956f40bd596bf4f85bd4d9c8
Rocketman3011 <dax.lundquist@gmail.com>
2026-04-03 15:57:22 -0700

    Create line_follower_bot_final.ino
    Added final code for line follower bot

---
+++ line_follower_bot_final/line_follower_bot_final.ino
@@ -0,0 +1,42 @@
+#include <SparkFun_TB6612.h>
+
+#define AIN1 2
+#define AIN2 4
+#define PWMA 3
+#define STBY 9
+#define SPEED 100
+#define BIN1 6
+#define BIN2 7
+#define PWMB 5
+#define leftSensor  A0
+#define rightSensor A1
+
+const int offsetA = -1;
+const int offsetB = -1;
+
+Motor leftMotor = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
+Motor rightMotor = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
+
+void setup() {
+  Serial.begin(9600);
+  delay(5000);
+}
+
+void loop(){
+  if (digitalRead(leftSensor) && digitalRead(rightSensor)) {
+    Serial.println("Stopping");
+    brake(leftMotor, rightMotor);
+  }
+  else if (digitalRead(leftSensor)) {
+    Serial.println("Turning Left");
+    left(leftMotor, rightMotor, SPEED);
+  }
+  else if (digitalRead(rightSensor)) {
+    Serial.println("Turning Right");
+    right(leftMotor, rightMotor, SPEED);
+  }
+  else if (!digitalRead(leftSensor) && !digitalRead(rightSensor)) {
+    Serial.println("Driving Forward");
+    forward(leftMotor, rightMotor, SPEED);
+  }
+}
